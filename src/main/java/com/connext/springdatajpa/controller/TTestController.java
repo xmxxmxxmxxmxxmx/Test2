5565ff1f0259782961b85d9e606cb6b8dc9022de
xmxxmxxmxxmxxmx <1695205904@qq.com>
2019-01-12 20:24:49 +0800

    test2添加ttestcontroller

---
+++ src/main/java/com/connext/springdatajpa/controller/TTestController.java
@@ -0,0 +1,11 @@
+package com.connext.springdatajpa.controller;
+
+/**
+ * @author xiamingxing
+ * @date 2019/1/12 20:22
+ */
+public class TTestController {
+    public String ttets(){
+        return "111";
+    }
+}
